7e0db5e137e94f6d6d9541799920968777435527
ww <1711300651@qq.com>
2017-08-30 17:34:38 +0800

    second

--- src/test/java/com/ww/RedisTest.java
+++ src/test/java/com/ww/RedisTest.java
@@ -1,24 +1,24 @@
-package com.ww;
-
-import com.ww.dao.RedisDao;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-/**
- * Created by ww on 17/8/25.
- */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RedisTest {
-    @Autowired
-    RedisDao redisDao;
-    @Test
-    public void test(){
-        redisDao.setValue("a","b");
-        String a = redisDao.getValue("a");
-        System.out.println(a);
-    }
-}
+//package com.ww;
+//
+//import com.ww.dao.RedisDao;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+///**
+// * Created by ww on 17/8/25.
+// */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class RedisTest {
+//    @Autowired
+//    RedisDao redisDao;
+//    @Test
+//    public void test(){
+//        redisDao.setValue("a","b");
+//        String a = redisDao.getValue("a");
+//        System.out.println(a);
+//    }
+//}
